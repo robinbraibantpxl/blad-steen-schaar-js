d10d2ca1de532681af9668f557db32017c80624e
Robin Braibant <robin.braibant@pxl.be>
2024-09-19 12:45:56 +0200

    Added logical structure

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// alert("Hello World")
+// Haal het element voor de computerkeuze op uit de HTML
+
+// Haal het element voor de spelerskeuze op uit de HTML
+
+// Haal de elementen met de mogelijke keuzes op uit de HTML
+
+// Haal het element om het resultaat te tonen op uit de HTML
+
+// Maak een variabele om de keuze van de gebruiker in bij te houden
+
+// Maak een variabele om de keuze van de computer in bij te houden
+
+// Maak een variabele om het resultaat in bij te houden
+
+// Schrijf de logica om ervoor te zorgen dat wanner er op een keuze (knop) wordt geklikt:
+// - De keuze van de gebruiker in de juiste variabele wordt gestoken
+// - De keuze van de gebruiker op het scherm wordt getoond in het juiste element
+// - De keuze van de computer willekeurig wordt gegenereerd
+// - Het juiste resultaat wordt getoond op het scherm
